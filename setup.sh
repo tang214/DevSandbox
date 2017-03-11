e4e3836dc7b6065a19b20bfc9fb066ebaceacc5b
Russell T. Sherman <russelltsherman@users.noreply.github.com>
2017-03-11 16:28:41 -0600

    always ok on new line (#33)

--- setup.sh
+++ setup.sh
@@ -29,55 +29,67 @@ if [ ! -d '.volumes' ]; then
   action "create shared data volume dir"
   mkdir .volumes
   mkdir .volumes/session
-  chmod 777 .volumes/session; ok
+  chmod 777 .volumes/session
+  ok
 fi
 
 if [ ! -f 'docker-compose.yml' ]; then
   action "writing docker-compose.yml"
-  sed -e s%/BURNINGFLIPSIDE%`pwd`%g docker-compose.tpl > docker-compose.yml; ok
+  sed -e s%/BURNINGFLIPSIDE%`pwd`%g docker-compose.tpl > docker-compose.yml
+  ok
 fi
 
 if [ ! -f 'src/secure_settings/class.FlipsideSettings.php' ]; then
   action "writing class.FlipsideSettings.php"
-  cp src/secure_settings/class.FlipsideSettings.example src/secure_settings/class.FlipsideSettings.php; ok
+  cp src/secure_settings/class.FlipsideSettings.example src/secure_settings/class.FlipsideSettings.php
+  ok
 fi
 
 if [ ! -f 'src/secure_settings/client_secret.json' ]; then
   action "writing client_secret.json"
-  cp src/secure_settings/client_secret.example src/secure_settings/client_secret.json; ok
+  cp src/secure_settings/client_secret.example src/secure_settings/client_secret.json
+  ok
 fi
 
 if [ ! -f 'src/secure_settings/aws.ini' ]; then
   action "writing aws.ini"
-  cp src/secure_settings/aws.example src/secure_settings/aws.ini; ok
+  cp src/secure_settings/aws.example src/secure_settings/aws.ini
+  ok
 fi
 
 action "fetch ldap image"
-docker pull burnerdev/openldap:1.1.8; ok
+docker pull burnerdev/openldap:1.1.8
+ok
 
 action "fetch mongo image"
-docker pull burnerdev/mongo:3.4; ok
+docker pull burnerdev/mongo:3.4
+ok
 
 warn "in a new terminal execute bin/seed to seed mongo database"
 warn "see deploy/mongo/SEEDME.md for detailed instructions"
 
 action "fetch mysql image"
-docker pull burnerdev/mysql:5.7; ok
+docker pull burnerdev/mysql:5.7
+ok
 
 warn "in a new terminal execute bin/seed to seed your mysql database"
 warn "see deploy/mysql/SEEDME.md for detailed instructions"
 
 action "fetch swagger editor image"
-docker pull swaggerapi/swagger-editor; ok
+docker pull swaggerapi/swagger-editor
+ok
 
 action "fetch php base image"
-docker pull php:7.1-apache; ok
+docker pull php:7.1-apache
+ok
 
 action "fetch web service image"
-docker pull burnerdev/php:7.1-apache; ok
+docker pull burnerdev/php:7.1-apache
+ok
 
 action "fetch wordpress image"
-docker pull burnerdev/wordpress:4.7.2; ok
+docker pull burnerdev/wordpress:4.7.2
+ok
 
 warn "once you've seeded your databases"
 warn "run docker-compose up"
