4da3a2a345dcb879aaaa295a617092382173aefc
Russell <russell.t.sherman@gmail.com>
2018-10-09 23:18:38 -0500

    centralize target and user data into custom commands

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,129 +1,142 @@
 /// <reference types="Cypress" />
 
-const url = require('url');
-const target = url.parse('https://localhost:3300')
+describe('user login', () => {
+  
+  // target holds a parsed url for the testes host
+  let target;
+  // object holds valid user credentials
+  let user;
 
-const VALID_USERNAME='developer'
-const VALID_EMAIL='burnerdev@burningflipside.com'
-const VALID_PASSWORD='p@s5w0rd'
-
-context('Location', () => {
   beforeEach(() => {
-    cy.visit(target.href)
-  })
-
-  it('cy.location() - get window.location', () => {
-    cy.location().should((location) => {
-      expect(location.href).to.eq(target.href)
-      expect(location.hostname).to.eq(target.hostname)
-      expect(location.origin).to.eq(`${target.protocol}//${target.host}`)
-      expect(location.pathname).to.eq('/')
-      expect(location.port).to.eq(target.port)
-      expect(location.protocol).to.eq(target.protocol)
+    // get target from support/commands
+    cy.target().then(($o) => {
+      target = $o
+    })
+    // get user from support/commands
+    cy.user().then(($o) => {
+      user = $o
     })
   })
 
-  it('cy.url() - get the current URL', () => {
-    cy.url().should('eq', `${target.href}`)
-  })
-})
-
-context('using login url', () => {
-  beforeEach(() => {
-    cy.visit(`${target.href}login.php`)
-  })
-
-  it('says Burning Flipside Profile Login', () => {
-    cy.get('body').contains('Burning Flipside Profile Login')
-  })
-
-  it('when submitting empty form', () => {
-    cy.get('#login_main_form').children('button[type=submit]').click()
-      // there is no visible feedback that an error has occurred     
+  context('Location', () => {
+    beforeEach(() => {
+      
+      cy.visit(target.href)
     })
-})
 
-context('using login modal', () => {
-  beforeEach(() => {
-    cy.visit(`${target.href}logout.php`)
-    cy.visit(`${target.href}`)
-
-    cy.server();
-    cy.route("POST", "/api/v1/login").as("authenticate");
-  })
-
-  it('says Burning Flipside Profile Login', () => {
-    cy.get('a').contains('Login').click()
-    cy.get('#login-dialog').contains('Login')
-  })
-
-  it('when submitting empty form', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('button[type=submit]').click()
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(403)
-      // there is no visible feedback that an error has occurred     
-    })
-  })
-
-  it('form can be submitted by pressing the enter key', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('input[name=username]').type('me@example.com{enter}')
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(403)
-      // there is no visible feedback that an error has occurred     
-    })
-  })
-
-  it('when submitting incomplete credentials', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('input[name=username]').type(VALID_USERNAME)
-    cy.get('#login_dialog_form').children('button[type=submit]').click()
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(403)
-      // there is no visible feedback that an error has occurred     
-    })
-  })
-
-  it('when submitting invalid credentials', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('input[name=username]').type(VALID_USERNAME)
-    cy.get('#login_dialog_form').children('input[name=password]').type('notmyrealpassword')
-    cy.get('#login_dialog_form').children('button[type=submit]').click()
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(403)
-      // there is no visible feedback that an error has occurred     
-    })
-  })
-
-  it('allows login using username', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('input[name=username]').type(VALID_USERNAME)
-    cy.get('#login_dialog_form').children('input[name=password]').type(VALID_PASSWORD)
-    cy.get('#login_dialog_form').children('button[type=submit]').click()
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(200)
-
+    it('cy.location() - get window.location', () => {
       cy.location().should((location) => {
         expect(location.href).to.eq(target.href)
+        expect(location.hostname).to.eq(target.hostname)
+        expect(location.origin).to.eq(`${target.protocol}//${target.host}`)
+        expect(location.pathname).to.eq('/')
+        expect(location.port).to.eq(target.port)
+        expect(location.protocol).to.eq(target.protocol)
       })
+    })
+
+    it('cy.url() - get the current URL', () => {
+      cy.url().should('eq', `${target.href}`)
     })
   })
 
-  it('allows login using email sddress', () => {    
-    cy.get('a').contains('Login').click()
-    cy.get('#login_dialog_form').children('input[name=username]').type(VALID_EMAIL)
-    cy.get('#login_dialog_form').children('input[name=password]').type(VALID_PASSWORD)
-    cy.get('#login_dialog_form').children('button[type=submit]').click()
-    cy.wait('@authenticate').then(function(xhr){
-      expect(xhr.status).to.eq(200)
+  context('using login url', () => {
+    beforeEach(() => {
+      cy.visit(`${target.href}login.php`)
+    })
 
-      cy.location().should((location) => {
-        expect(location.href).to.eq(target.href)
+    it('says Burning Flipside Profile Login', () => {
+      cy.get('body').contains('Burning Flipside Profile Login')
+    })
+
+    it('when submitting empty form', () => {
+      cy.get('#login_main_form').children('button[type=submit]').click()
+        // there is no visible feedback that an error has occurred     
+      })
+  })
+
+  context('using login modal', () => {
+    beforeEach(() => {
+      cy.visit(`${target.href}logout.php`)
+      cy.visit(`${target.href}`)
+
+      cy.server();
+      cy.route("POST", "/api/v1/login").as("authenticate");
+    })
+
+    it('says Burning Flipside Profile Login', () => {
+      cy.get('a').contains('Login').click()
+      cy.get('#login-dialog').contains('Login')
+    })
+
+    it('when submitting empty form', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('button[type=submit]').click()
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(403)
+        // there is no visible feedback that an error has occurred     
       })
     })
+
+    it('form can be submitted by pressing the enter key', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('input[name=username]').type('me@example.com{enter}')
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(403)
+        // there is no visible feedback that an error has occurred     
+      })
+    })
+
+    it('when submitting incomplete credentials', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('input[name=username]').type(user.username)
+      cy.get('#login_dialog_form').children('button[type=submit]').click()
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(403)
+        // there is no visible feedback that an error has occurred     
+      })
+    })
+
+    it('when submitting invalid credentials', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('input[name=username]').type(user.username)
+      cy.get('#login_dialog_form').children('input[name=password]').type('notmyrealpassword')
+      cy.get('#login_dialog_form').children('button[type=submit]').click()
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(403)
+        // there is no visible feedback that an error has occurred     
+      })
+    })
+
+    it('allows login using username', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('input[name=username]').type(user.username)
+      cy.get('#login_dialog_form').children('input[name=password]').type(user.password)
+      cy.get('#login_dialog_form').children('button[type=submit]').click()
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(200)
+
+        cy.location().should((location) => {
+          expect(location.href).to.eq(target.href)
+        })
+      })
+    })
+
+    it('allows login using email sddress', () => {    
+      cy.get('a').contains('Login').click()
+      cy.get('#login_dialog_form').children('input[name=username]').type(user.email)
+      cy.get('#login_dialog_form').children('input[name=password]').type(user.password)
+      cy.get('#login_dialog_form').children('button[type=submit]').click()
+      cy.wait('@authenticate').then(function(xhr){
+        expect(xhr.status).to.eq(200)
+
+        cy.location().should((location) => {
+          expect(location.href).to.eq(target.href)
+        })
+      })
+    })
+
   })
 
 })
-
 // https://docs.cypress.io/api/api/table-of-contents.html
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,43 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+const url = require('url');
+
+// this will need to be exapnded to accomodate multiple targets
+// perhaps this command can accept a parameter to return the desired url?
+Cypress.Commands.add('target', () => {
+  // this should probably be set in ENV
+  return url.parse('https://localhost:3300');
+})
+
+Cypress.Commands.add('user', () => {
+  // this should probably be set in ENV
+  return {
+    email: 'burnerdev@burningflipside.com',
+    username: 'developer',
+    password: 'p@s5w0rd'
+  };
+})
+
+// perform login by posting directly to API
+Cypress.Commands.add('login', () => {
+  cy.target().then(($o) => {
+    const target = $o
+
+    cy.user().then(($o) => {
+      const user = $o
+
+      cy.request({
+        method: 'POST',
+        url: `${target.href}api/v1/login`,
+        body: {
+          username: user.username,
+          password: user.password
+        }
+      })
+      .then((resp) => {
+        console.log(resp)
+      })
+    })
+  })
+})
